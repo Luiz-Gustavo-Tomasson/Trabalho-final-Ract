4b940c4b9f5ae16e3180cc4c7c70b45fbfcd2af0
Luiz-Gustavo-Tomasson <139566913+Luiz-Gustavo-Tomasson@users.noreply.github.com>
2023-10-01 14:58:20 -0300

    Update register.tsx

--- register.tsx
+++ register.tsx
@@ -1,9 +1,9 @@
 import * as yup from 'yup';
 
 export const registerSchema = yup.object().shape({
-    name: yup.string().required("Nome é obrigatório"),
+    nome: yup.string().required("Nome é obrigatório"),
     email: yup.string().email("E-mail inválido").required("E-mail é obrigatório"),
-    age: yup.string().required("Idade é obrigatória"),
-    addres: yup.string().required("Endereço é obrigatório"),
+    telefone: yup.string().required("Telefone é obrigatória"),
+    observacoes: yup.string().required("Observações é obrigatório"),
 
 })
